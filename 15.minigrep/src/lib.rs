1677cb9904883f1c5a8716e643851187478d1d8f
Gu-Miao <1450206741@qq.com>
2022-10-18 18:20:32 +0800

    Update 15.minigrep

--- 15.minigrep/src/lib.rs
+++ 15.minigrep/src/lib.rs
@@ -20,13 +20,19 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
+  pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
     if args.len() < 3 {
       return Err("not enough arguments!");
     }
-
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    args.next();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Don't get a query string"),
+    };
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Don't get a file name"),
+    };
 
     // 在 powershell 中，通过 $env:CASE_INSENSITIVE=1 设置环境变量，再执行
     // cargo run to poem.txt 测试
@@ -41,28 +47,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-  let mut result = vec![];
-
-  for line in content.lines() {
-    if line.contains(query) {
-      result.push(line);
-    }
-  }
-
-  result
+  content
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-  let mut result = vec![];
-  let query = query.to_lowercase();
-
-  for line in content.lines() {
-    if line.to_lowercase().contains(&query) {
-      result.push(line);
-    }
-  }
-
-  result
+  content
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+    .collect()
 }
 
 #[cfg(test)]
--- 15.minigrep/src/main.rs
+++ 15.minigrep/src/main.rs
@@ -2,11 +2,8 @@ use minigrep::{run, Config};
 use std::{env, process};
 
 fn main() {
-  // 如果需要接受非法 unicode 字符，可以使用 env::args_os 方法，它会
-  // 返回 OsString
-  let args: Vec<String> = env::args().collect();
-
-  let config = Config::new(&args).unwrap_or_else(|err| {
+  // 如果需要接受非法 unicode 字符，可以使用 env::args_os 方法，它会返回 OsString
+  let config = Config::new(env::args()).unwrap_or_else(|err| {
     // eprintln 宏会将内容打印到标准错误流中
     eprintln!("Problem parsing arguments: {}", err);
     process::exit(1);
